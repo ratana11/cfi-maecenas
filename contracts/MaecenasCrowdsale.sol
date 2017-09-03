49c4cbad5074e55fc626bca97a4c37509a1d8015
erika <erika@serox.si>
2017-09-03 20:34:13 +0200

    Updating values

--- contracts/MaecenasCrowdsale.sol
+++ contracts/MaecenasCrowdsale.sol
@@ -4,10 +4,10 @@ import "./Crowdsale.sol";
 
 contract MaecenasCrowdsale is Crowdsale {
   function MaecenasCrowdsale(){
-    presaleStartBlock = 0; //TODO: Set proper block number
-    presaleUnlimitedStartBlock = 0; //TODO: Set proper block number
-    crowdsaleStartBlock = 0; //TODO: Set proper block number
-    crowdsaleEndedBlock = 0; //TODO: Set proper block number
+    presaleStartBlock = 4241483;
+    presaleUnlimitedStartBlock = 4245055;
+    crowdsaleStartBlock = 4248627;
+    crowdsaleEndedBlock = 4348635;
 
     minCap = 9375 * 10**18;
     maxP1Cap = 31250 * 10**18;
@@ -17,6 +17,6 @@ contract MaecenasCrowdsale is Crowdsale {
 
     maxTokenSupply = 100000000 * 10**18;
     cofounditReward = 4000000 * 10**18;
-    cofounditAddress = 0x0;
+    cofounditAddress = 0x988c3eA5554f3D2fB5ECB4dC5c35126eEf3B8a5D;
   }
 }
--- migrations/3_deploy_Token.js
+++ migrations/3_deploy_Token.js
@@ -5,5 +5,5 @@ var SafeMathLib = artifacts.require("./Utils/SafeMath.sol");
 module.exports = function(deployer) {
   deployer.deploy(SafeMathLib);
   deployer.link(SafeMathLib, Token);
-  deployer.deploy(Token, Crowdsale.deployed().then(function(crowdsaleInsance){return crowdsaleInsance.address}), 0); //TODO: set Token Lock
+  deployer.deploy(Token, Crowdsale.deployed().then(function(crowdsaleInsance){return crowdsaleInsance.address}), 4366494);
 };
